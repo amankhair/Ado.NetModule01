dbe27bb7fb1722bec0d904127c735ed93f4d23e1
Amankeldi <amankhair38@gmail.com>
2018-01-14 17:14:46 +0600

    -

--- AdoNetModule01/MainWindow.xaml.cs
+++ AdoNetModule01/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdoNetModule01
 {
@@ -20,9 +8,38 @@ namespace AdoNetModule01
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string connectionString = "";
+        //string connStringOledb = "";
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            connectionString = @"Data Source=DESKTOP-CJP4DOA;Initial Catalog=MCS;Integrated Security=True";
+        }
+
+        private void ConnectToServerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                ConnectMessage.Text += "Подключение открыто: " + conn.ConnectionString + "\n\n";
+                ConnectMessage.Text += "Подключение БД: " + conn.Database + "\n\n";
+                ConnectMessage.Text += "Серввер подключения: " + conn.DataSource + "\n\n";
+                ConnectMessage.Text += "Версия сервера: " + conn.ServerVersion + "\n\n";
+                ConnectMessage.Text += "Состояние сервера: " + conn.State + "\n\n";
+                ConnectMessage.Text += "Время ожидания: " + conn.ConnectionTimeout + "\n\n";
+            }
+        }
+
+        private void ConnectToServer_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
